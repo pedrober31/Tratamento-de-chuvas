b80fc05cfe8c891720080f4e69dc0a962c783c49
pedrober31 <78823560+pedrober31@users.noreply.github.com>
2022-09-06 09:59:42 -0300

    fix: prevent errors in the KMeans

--- Cluster_KMeans.py
+++ Cluster_KMeans.py
@@ -1,17 +1,38 @@
+from sklearn.metrics import davies_bouldin_score
+from sklearn.cluster import KMeans
 import hydrological_signatures
+import pandas as pd
+
+df_assinaturas = hydrological_signatures.result()
 
 def padronizar_assinaturas(df_assinatura):
     """
     df_assinatura: Dataframe contendo as assinaturas hidrológicas por estação
     return: Novo DataFrame com as assinaturas padronizadas
     """
+    df_assinatura = df_assinatura.dropna(axis=1)
+    df_assinatura = df_assinatura.T
+
     for c in df_assinatura.columns:
-        if df_assinatura[c].std() != 0:
-            df_assinatura[c] = (df_assinatura[c]-df_assinatura[c].mean())/df_assinatura[c].std()
-        else:
-            pass
+        df_assinatura[c] = (df_assinatura[c]-df_assinatura[c].mean())/df_assinatura[c].std()
     
     return df_assinatura
 
-df_assinaturas = hydrological_signatures.result()
-# print(padronizar_assinaturas(df_assinaturas))
+df_assinaturas = padronizar_assinaturas(df_assinaturas)
+df_assinaturas.dropna(axis=1, inplace=True)
+
+def clustering_kmeans(data):
+    classes = []
+    db = []
+
+    for i in range(5):
+        classes.append(i+2)
+        kmeans = KMeans(n_clusters=i+2).fit(data)
+        labels = kmeans.labels_
+        db.append(davies_bouldin_score(data, labels))
+
+    db_k = pd.DataFrame({'NClusters':classes,'DB - K means':db})
+    db_k.set_index('NClusters', inplace=True)
+
+    return db_k
+
